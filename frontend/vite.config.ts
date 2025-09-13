059f6c9730cd03820eed4796d550402c45e80bf6
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-13 14:31:55 +0200

    Config update + dependencies setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [tailwindcss(), react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  },
 })
