c834e55c823464c33ae325f482d61ff2359fff77
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-13 20:52:09 +0200

    Cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContextProvider";
 import { useAuth } from "./hooks/useAuth";
