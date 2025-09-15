571de52e477070caf04ea0795fda4d1478e69c81
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-09-15 21:25:17 +0200

    Cleanup

--- frontend/src/context/authContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-interface AuthContextType {
-    token: string;
-    updateToken: (token: string) => void;
-}
-
-export const AuthContext = createContext<AuthContextType | undefined>(undefined);
